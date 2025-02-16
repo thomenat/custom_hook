2f3abd726300931d2414e1791a802f63b806f353
˜nthome˜ <nataliathome@gmail.com>
2025-02-16 19:20:48 +0000

    created personalized custom hook

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import UseFetch from "./UseFetch";
+
+function FetchYogaData() {
+    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
+
+    return (
+        <>
+         <ul className='list_data_main'>
+            {data && data.map((e)=>(
+          <>
+            <li className='list_data'>
+                <h3>{e.name}</h3>
+                <p><strong>Benefits: </strong>{e.benefits}</p>
+                </li>      
+                </>
+        ))}
+         </ul>
+        </>
+      )
+    }
+    
+export default FetchYogaData;
